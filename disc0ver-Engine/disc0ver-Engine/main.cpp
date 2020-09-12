f0e339374908309d7753a98925bc59a89c2743ec
leo6033 <long452a@163.com>
2020-09-13 00:52:16 +0800

    OpenGL create window

--- disc0ver-Engine/disc0ver-Engine/main.cpp
+++ disc0ver-Engine/disc0ver-Engine/main.cpp
@@ -1,6 +1,52 @@
+/*
+ * @Description: 
+ * @Author: НэПл
+ * @Email: long452a@163.com
+ * @Date: 2020-09-12
+ */
+
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include<iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 int main() {
+	glfwInit();
+	GLFWwindow* window = glfwCreateWindow(800, 800, "disc0ver", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Fail to create window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
