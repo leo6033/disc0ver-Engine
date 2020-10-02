c6ccb645502381b04c8c81bfd476229b363caffc
leo6033 <long452a@163.com>
2020-10-02 16:16:23 +0800

    file: create transform file

---
+++ disc0ver-Engine/disc0ver-Engine/Render/transform.cpp
@@ -0,0 +1,8 @@
+/*
+ * @Description:
+ * @Author: НэПл
+ * @Email: long452a@163.com
+ * @Date: 2020-10-02
+ */
+
+#include "transform.h"
---
+++ disc0ver-Engine/disc0ver-Engine/Render/transform.h
@@ -0,0 +1,18 @@
+/*
+ * @Description:
+ * @Author: НэПл
+ * @Email: long452a@163.com
+ * @Date: 2020-10-02
+ */
+
+#pragma once
+#ifndef TRANSFORM_H
+
+
+namespace disc0ver {
+	class Transform {
+
+	};
+}
+
+#endif // !TRANSFORM_H
